9dbf197c391c2a57925f633a3f94fb0568fba8f6
shenfive@gmail.com <Danny Shen>
2022-10-29 14:23:10 +0800

    登出

--- a209EmailAuth/ViewController.swift
+++ a209EmailAuth/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user{
+//                self.statusLabel.text = "歡迎：" + (user.displayName ?? "未知")
+                print(user.email)
+//                self.signOutBtn.isEnabled = true
+//                self.gSingInButton.isEnabled = false
+            }else{
+                print("未登入")
+//                self.statusLabel.text = "未登入"
+//                self.signOutBtn.isEnabled = false
+//                self.gSingInButton.isEnabled = true
+            }
+        }
+        
+        
+        
+        
     }
 
     @IBAction func goCreateAccount(_ sender: Any) {
@@ -19,5 +37,15 @@ class ViewController: UIViewController {
         self.present(nextVC, animated: true)
     }
     
+    
+    
+    @IBAction func singOut(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+        }catch{
+            print(error.localizedDescription)
+        }
+        
+    }
 }
 
